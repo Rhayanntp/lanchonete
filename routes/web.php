b224fec8b3f90668cfd118f19aecbd1a5706b78c
Rhayanntp <rhayanrhangel58@gmail.com>
2025-03-25 14:59:08 -0300

    web

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Models\Cliente;
+use GuzzleHttp\Promise\Create;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/cadastrar/cliente', Create::class);
